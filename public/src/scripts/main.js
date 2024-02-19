d6ab5e035e7840ccb790c93161643a14bd4bd8cd
Jason-Goon <156106640+Jason-Goon@users.noreply.github.com>
2024-02-19 15:19:25 +0200

    Add files via upload

---
+++ public/src/scripts/main.js
@@ -0,0 +1,27 @@
+// src/scripts/main.js
+
+// Smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+  
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu-toggle');
+    const menu = document.getElementById('mobile-menu');
+
+    menuButton.addEventListener('click', function() {
+      menu.classList.toggle('hidden');
+    });
+  });
+
+
+
+// Form validation
